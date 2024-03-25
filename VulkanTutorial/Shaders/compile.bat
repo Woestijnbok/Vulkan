22ae7f647a6f4568b3a7b1af50cb49ec6ba861fd
Artuur Demeyer <Artuur.Demeyer@Outlook.com>
2024-03-25 15:10:11 +0100

    bat file works now without the full path

--- VulkanTutorial/Shaders/compile.bat
+++ VulkanTutorial/Shaders/compile.bat
@@ -1,3 +1,3 @@
-D:\Vulkan\1.3.275.0\SDK\Bin\glslc.exe Basic.vert -o vert.spv
-D:\Vulkan\1.3.275.0\SDK\Bin\glslc.exe Basic.frag -o frag.spv
+glslc.exe Basic.vert -o vert.spv
+glslc.exe Basic.frag -o frag.spv
 pause
